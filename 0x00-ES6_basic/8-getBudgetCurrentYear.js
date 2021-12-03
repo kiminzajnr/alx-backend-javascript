b9a5372ce35ce612f31cfde6faba71de65f6dd93
Erick Kiminza <kiminzaeric@gmail.com>
2021-12-03 10:38:49 +0300

    fix esLint

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -4,11 +4,11 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  let budget = {
+  const budget = {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
-    [`capita-${getCurrentYear()}`]: capita
-}
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 
   return budget;
 }
