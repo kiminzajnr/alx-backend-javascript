9400bac46606191e1abf958e622bbdbcedb95cb3
Erick Kiminza <kiminzaeric@gmail.com>
2021-12-06 15:14:18 +0300

    add task 5

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+// eslint-disable-next-line
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(amount_) {
+    this._amount = amount_;
+  }
+
+  set currency(currency_) {
+    this._currency = currency_;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return `${amount} * ${conversionRate}`;
+  }
+}
